a90674a4d9d34176c9938760b160ffff55df810c
jansulja <shuky11@rocketmail.com>
2017-01-24 21:18:49 +0100

    Added karma configuration file, test for payment controller, fixed bugs in payment controller and view.

--- SEP-Bolt Insurance Group/com.bolt.insurance.group.app/src/main/resource/static/app/components/insurance/payment/payment.controller.js
+++ SEP-Bolt Insurance Group/com.bolt.insurance.group.app/src/main/resource/static/app/components/insurance/payment/payment.controller.js
@@ -5,50 +5,55 @@
 		.module('bolt-insurance-group.insurance.payment')
 		.controller('PaymentInsuranceController', PaymentInsuranceController);
 
-	PaymentInsuranceController.$inject = ['$scope','localStorageService', '$state', 'InsuranceProgress'];
-	function PaymentInsuranceController($scope,localStorageService, $state, InsuranceProgress) {
+	PaymentInsuranceController.$inject = ['localStorageService', '$state', 'InsuranceProgress'];
+	function PaymentInsuranceController(localStorageService, $state, InsuranceProgress) {
 
 		var pic = this;
 		
-		$scope.world = localStorageService.cookie.get('world');
-		$scope.money = localStorageService.cookie.get('money');
-		$scope.kids = localStorageService.cookie.get('kids');
-		$scope.grownups = localStorageService.cookie.get('grownups');
-		$scope.olds = localStorageService.cookie.get('olds');
-		$scope.dt1 = new Date(parseInt(localStorageService.cookie.get('date1')));
-		$scope.dt2 = new Date(parseInt(localStorageService.cookie.get('date2')));
-		$scope.sportCheckBox = localStorageService.cookie.get('sportCheckBox');
-		$scope.selectedSport = localStorageService.cookie.get('selectedSport');
-		$scope.homeCheckBox = localStorageService.cookie.get('homeCheckBox');
-		$scope.roadCheckBox = localStorageService.cookie.get('roadCheckBox');
-		$scope.towing = localStorageService.cookie.get('towingCheckBox');
-		$scope.repair = localStorageService.cookie.get('repairCheckBox');
-		$scope.hotel = localStorageService.cookie.get('hotelCheckBox');
-		$scope.alternative = localStorageService.cookie.get('alternativeCheckBox');
-		$scope.fire = localStorageService.cookie.get('fireCheckBox');
-		$scope.flood = localStorageService.cookie.get('floodCheckBox');
-		$scope.theft = localStorageService.cookie.get('theftCheckBox');
-		$scope.earthshaker = localStorageService.cookie.get('earthshakerCheckBox');
-		$scope.amount = parseFloat(localStorageService.cookie.get('amount'));
-		$scope.totalPrice = ($scope.amount + $scope.amount*0.2) / 120;
-		$scope.homeArea = localStorageService.cookie.get('homeArea');
-		$scope.ageOfHome = localStorageService.cookie.get('ageOfHome');
-		$scope.estimatedValueOfHome = localStorageService.cookie.get('estimatedValueOfHome');
-		$scope.homeOwnerName = localStorageService.cookie.get('homeOwnerName');
-		$scope.homeOwnerSurname = localStorageService.cookie.get('homeOwnerSurname');
-		$scope.homeOwnerJmbg = localStorageService.cookie.get('homeOwnerJmbg');
-		$scope.homeAdress = localStorageService.cookie.get('homeAdress');
-		$scope.vehicleOwnerName = localStorageService.cookie.get('vehicleOwnerName');
-		$scope.vehicleOwnerSurname = localStorageService.cookie.get('vehicleOwnerSurname');
-		$scope.vehicleOwnerJmbg = localStorageService.cookie.get('vehicleOwnerJmbg');
-		$scope.vehicleType = localStorageService.cookie.get('vehicleType');
-		$scope.vehicleYear = localStorageService.cookie.get('vehicleYear');
-		$scope.vehiclePlates = localStorageService.cookie.get('vehiclePlates');
-		$scope.vehicleChassis = localStorageService.cookie.get('vehicleChassis');
+		pic.world = localStorageService.cookie.get('world');
+		pic.money = localStorageService.cookie.get('money');
+		pic.kids = localStorageService.cookie.get('kids');
+		pic.grownups = localStorageService.cookie.get('grownups');
+		pic.olds = localStorageService.cookie.get('olds');
+		pic.dt1 = new Date(parseInt(localStorageService.cookie.get('date1')));
+		pic.dt2 = new Date(parseInt(localStorageService.cookie.get('date2')));
+		pic.sportCheckBox = localStorageService.cookie.get('sportCheckBox');
+		pic.selectedSport = localStorageService.cookie.get('selectedSport');
+		pic.homeCheckBox = localStorageService.cookie.get('homeCheckBox');
+		pic.roadCheckBox = localStorageService.cookie.get('roadCheckBox');
+		pic.towing = localStorageService.cookie.get('towingCheckBox');
+		pic.repair = localStorageService.cookie.get('repairCheckBox');
+		pic.hotel = localStorageService.cookie.get('hotelCheckBox');
+		pic.alternative = localStorageService.cookie.get('alternativeCheckBox');
+		pic.fire = localStorageService.cookie.get('fireCheckBox');
+		pic.flood = localStorageService.cookie.get('floodCheckBox');
+		pic.theft = localStorageService.cookie.get('theftCheckBox');
+		pic.earthshaker = localStorageService.cookie.get('earthshakerCheckBox');
+		pic.amount = parseFloat(localStorageService.cookie.get('amount'));
+		pic.totalPrice = (pic.amount + pic.amount*0.2) / 120;
+		pic.homeArea = localStorageService.cookie.get('homeArea');
+		pic.ageOfHome = localStorageService.cookie.get('ageOfHome');
+		pic.estimatedValueOfHome = localStorageService.cookie.get('estimatedValueOfHome');
+		pic.homeOwnerName = localStorageService.cookie.get('homeOwnerName');
+		pic.homeOwnerSurname = localStorageService.cookie.get('homeOwnerSurname');
+		pic.homeOwnerJmbg = localStorageService.cookie.get('homeOwnerJmbg');
+		pic.homeAdress = localStorageService.cookie.get('homeAdress');
+		pic.vehicleOwnerName = localStorageService.cookie.get('vehicleOwnerName');
+		pic.vehicleOwnerSurname = localStorageService.cookie.get('vehicleOwnerSurname');
+		pic.vehicleOwnerJmbg = localStorageService.cookie.get('vehicleOwnerJmbg');
+		pic.vehicleType = localStorageService.cookie.get('vehicleType');
+		pic.vehicleYear = localStorageService.cookie.get('vehicleYear');
+		pic.vehiclePlates = localStorageService.cookie.get('vehiclePlates');
+		pic.vehicleChassis = localStorageService.cookie.get('vehicleChassis');
 	
 		
-		//var current = (vic.homeCheckBox)?5:4;
-		InsuranceProgress.setCurrent(6);
+	
+		
+		var current = (pic.homeCheckBox && pic.roadCheckBox)?6:
+			(!pic.homeCheckBox && !pic.roadCheckBox)?4:5;
+		
+		
+		InsuranceProgress.setCurrent(current);
 		
 		pic.back = function() {
 			var home = localStorageService.cookie.get('homeCheckBox');
---
+++ SEP-Bolt Insurance Group/com.bolt.insurance.group.app/src/main/resource/static/tests/karma.conf.js
@@ -0,0 +1,127 @@
+// Karma configuration
+// Generated on Mon Jan 23 2017 18:51:51 GMT+0100 (Central Europe Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '../',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'assets/js/modernizr-2.6.2.min.js',
+        'assets/js/angular/angular.js',
+         'assets/js/angular-ui-router/release/angular-ui-router.js',
+        'assets/js/angular-scroll/angular-scroll.js',
+        'assets/js/angular-scroll-animate/dist/angular-scroll-animate.js',
+         'assets/js/angular-bootstrap/ui-bootstrap.js',
+        'assets/js/angular-bootstrap/ui-bootstrap-tpls.js',
+        'assets/js/angular-animate/angular-animate.js',
+        'assets/js/lodash/lodash.js',
+        'assets/js/restangular/src/restangular.js',
+        'assets/js/angularjs-datepicker/src/js/angular-datepicker.js',
+        'assets/js/angular-translate/angular-translate.js',
+        'assets/js/angular-sanitize/angular-sanitize.js',
+        'assets/js/angular-fancy-modal/dist/angular-fancy-modal.js',
+        'assets/js/angular-local-storage/dist/angular-local-storage.js',
+        'assets/js/jquery-1.11.0.min.js',
+        'assets/js/bootstrap.min.js',
+        'assets/js/waypoints.min.js',
+        'assets/js/owl.carousel.min.js',
+        'assets/js/jquery.scrollTo.min.js',
+        'assets/js/front.js',
+        'assets/js/angular-mocks/angular-mocks.js',
+        'app/app.module.js',
+'app/components/core/core.module.js',
+'app/components/core/core.route.js',
+'app/components/core/core.init.js',
+'app/components/core/core.controller.js',
+ 'app/components/insurance/insurance.module.js',
+'app/components/insurance/insurance.controller.js',
+'app/components/insurance/insurance.route.js',
+'app/components/insurance/insurance.service.js',
+'app/components/insurance/insurance.service.spec.js',
+//'app/components/insurance/insurance.controller.spec.js',
+'app/components/insurance/user-modal/user-modal.module.js',
+'app/components/insurance/user-modal/user-modal.factory.js',
+'app/components/insurance/user-modal/user-modal.controller.js',
+'app/components/insurance/user-modal/user-modal.service.js',
+'app/components/insurance/home-modal/home-modal.module.js',
+'app/components/insurance/home-modal/home-modal.factory.js',
+'app/components/insurance/home-modal/home-modal.controller.js',
+'app/components/insurance/home-modal/home-modal.service.js',
+'app/components/insurance/vehicle-modal/vehicle-modal.module.js',
+'app/components/insurance/vehicle-modal/vehicle-modal.factory.js',
+'app/components/insurance/vehicle-modal/vehicle-modal.controller.js',
+'app/components/insurance/vehicle-modal/vehicle-modal.service.js',
+'app/components/insurance/total-price/total-price.module.js',
+'app/components/insurance/total-price/total-price.controller.js',
+'app/components/insurance/insurance-users/insurance-users.module.js',
+'app/components/insurance/insurance-users/insurance-users.controller.js',
+'app/components/insurance/payment-response/payment-response.module.js',
+'app/components/insurance/payment-response/payment-response.controller.js',
+'app/components/insurance/payment/payment.module.js',
+'app/components/insurance/payment/payment.controller.js',
+'app/components/insurance/payment/payment.controller.spec.js',
+'app/shared/i18n/i18n.module.js',
+'app/shared/i18n/i18n.config.js',
+'app/shared/paypal/paypal-button.directive.js',
+'app/shared/progress-bar/progress-bar.directive.js',
+'app/shared/progress-bar/progress-bar.service.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
