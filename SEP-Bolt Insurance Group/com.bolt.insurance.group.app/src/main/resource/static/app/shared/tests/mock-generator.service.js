fc30397b5b5e21c56dbd47726ce48b2cfc9c62b1
jansulja <shuky11@rocketmail.com>
2017-01-26 19:55:43 +0100

    Unit test for UserModalController, added new mock services to MockGenerator service

--- SEP-Bolt Insurance Group/com.bolt.insurance.group.app/src/main/resource/static/app/shared/tests/mock-generator.service.js
+++ SEP-Bolt Insurance Group/com.bolt.insurance.group.app/src/main/resource/static/app/shared/tests/mock-generator.service.js
@@ -1,137 +1,148 @@
 (function() {
 	"use strict";
 
-	angular
-		.module('bolt-insurance-group.insurance')
-		.service('MockGenerator', MockGenerator);
+	angular.module('bolt-insurance-group.insurance').service('MockGenerator',
+			MockGenerator);
 
-	MockGenerator.$inject = ['$q'];
+	MockGenerator.$inject = [ '$q' ];
 	function MockGenerator($q) {
-		
-		    
+
 		return {
-			
-			InsuranceProgressMock:function(){
-				
+
+			InsuranceProgressMock : function() {
+
 				return {
-					setSteps:function(){},
-					setCurrent:function(){},
-					addStep:function(){},
-					removeStep:function(){}
+					setSteps : function() {
+					},
+					setCurrent : function() {
+					},
+					addStep : function() {
+					},
+					removeStep : function() {
+					}
 				}
-				
-				
+
 			},
-		
-		
-			localStorageServiceMock:function(){
+
+			localStorageServiceMock : function() {
 				return {
-					
-					cookie: {
-						count:0,
-						isSupported:true,
-						get: function(mock){
-							
+
+					cookie : {
+						count : 0,
+						isSupported : true,
+						get : function(mock) {
+
 							var obj = this.storage[mock];
-							
-							if(typeof obj !== 'undefined'){
+
+							if (typeof obj !== 'undefined') {
 								return obj;
-							}else{
+							} else {
 								return 'mock value ' + this.count++;
 							}
-						
+
 						},
-						storage:{
-							
-							money:'',
-							kids:'',
-							grownups:'',
-							olds:'',
-							date1:'',
-							date2:'',
-							sportCheckBox:false,
-							selectedSport:false,
-							homeCheckBox:false,
-							roadCheckBox:false,
-							towingCheckBox:false,
-							repairCheckBox:false,
-							hotelCheckBox:false,
-							alternativeCheckBox:false,
-							fireCheckBox:false,
-							floodCheckBox:false,
-							theftCheckBox:false,
-							earthshakerCheckBox:false,
-							homeArea:'',
-							ageOfHome:'',
-							estimatedValueOfHome:'',
-							kidsNumber:'0',
-							grownupsNumber:'0',
-							oldsNumber:'0',
-							listOfUsers:[]
+						storage : {
+
+							money : '',
+							kids : '',
+							grownups : '',
+							olds : '',
+							date1 : '',
+							date2 : '',
+							sportCheckBox : false,
+							selectedSport : false,
+							homeCheckBox : false,
+							roadCheckBox : false,
+							towingCheckBox : false,
+							repairCheckBox : false,
+							hotelCheckBox : false,
+							alternativeCheckBox : false,
+							fireCheckBox : false,
+							floodCheckBox : false,
+							theftCheckBox : false,
+							earthshakerCheckBox : false,
+							homeArea : '',
+							ageOfHome : '',
+							estimatedValueOfHome : '',
+							kidsNumber : '0',
+							grownupsNumber : '0',
+							oldsNumber : '0',
+							listOfUsers : []
 						},
-						set: function(name,value,a,b){
-							
+						set : function(name, value, a, b) {
+
 							this.storage[name] = value;
-							
+
 						}
-				
+
 					}
-					
+
 				}
 			},
-			
-			$stateMock:function(){
+
+			$stateMock : function() {
 				return {
-					current:'payment',
-					go:function(newState){
-						this.current = newState; 
+					current : 'payment',
+					go : function(newState) {
+						this.current = newState;
 					}
-				
+
 				};
-				
-				
+
 			},
-			
-			$translateMock:function(){
+
+			$translateMock : function() {
 				return {
-					current:'en',
-					use:function(key){
-						if(key){
+					current : 'en',
+					use : function(key) {
+						if (key) {
 							this.current = key;
-						}else{
+						} else {
 							return this.current;
 						}
-						
-					}
-					
-				};
-				
-				
-			},
-			
-			$fancyModalMock:function(){
-				return {
-					open:function(obj){
-						
-					}
-				
-				};
-				
-				
-			},
-			
 
-			
-			userModalMock:function(){
-				// timeout returns promise
-				return{
-					open:function(){
 					}
-				}	
+
+				};
+
+			},
+
+			$fancyModalMock : function() {
+				return {
+					open : function(obj) {
+
+					}
+
+				};
+
+			},
+
+			userModalMock : function() {
+				// timeout returns promise
+				return {
+					open : function() {
+					}
+				}
+			},
+
+			$uibModalInstanceMock : function() {
+				return {
+					dismiss:function(){},
+					close:function(){}
+				}
+			},
+			itemsMock : function() {
+				return {
+					status : 'new'
+				}
+			},
+			UserMock : function() {
 			}
+
+		// $uibModalInstance, items, User
+
 		}
-			
-		
+
 	}
-	
+
 })();
